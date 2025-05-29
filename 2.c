6308fa0aa5b8e93ecd384e1c5c06b0e57befcc2d
Sri Sayee <71455470+ksrisayee12@users.noreply.github.com>
2025-05-30 00:19:09 +0530

    Add files via upload

--- 2.c
+++ 2.c
@@ -480,3 +480,140 @@
 //     printf("\n");
 //     return 0;
 // }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// // Define the node structure
+// struct Node {
+//     int data;
+//     struct Node* next;
+// };
+
+// struct Node* head = NULL;
+
+// // Insert at the end
+// void insert(int value) {
+//     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+//     newNode->data = value;
+//     newNode->next = NULL;
+
+//     if (head == NULL) {
+//         head = newNode;
+//     } else {
+//         struct Node* temp = head;
+//         while (temp->next != NULL) {
+//             temp = temp->next;
+//         }
+//         temp->next = newNode;
+//     }
+
+//     printf("%d inserted.\n", value);
+// }
+
+// // Delete from the beginning
+// void delete() {
+//     if (head == NULL) {
+//         printf("List is empty.\n");
+//     } else {
+//         struct Node* temp = head;
+//         head = head->next;
+//         printf("%d deleted.\n", temp->data);
+//         free(temp);
+//     }
+// }
+
+// // Display the list
+// void display() {
+//     if (head == NULL) {
+//         printf("List is empty.\n");
+//     } else {
+//         struct Node* temp = head;
+//         printf("Linked List: ");
+//         while (temp != NULL) {
+//             printf("%d -> ", temp->data);
+//             temp = temp->next;
+//         }
+//         printf("NULL\n");
+//     }
+// }
+
+// // Main menu
+// int main() {
+//     int choice, value;
+//     while (1) {
+//         printf("\n1. Insert\n2. Delete\n3. Display\n4. Exit\n");
+//         printf("Enter choice: ");
+//         scanf("%d", &choice);
+
+//         switch (choice) {
+//             case 1:
+//                 printf("Enter value to insert: ");
+//                 scanf("%d", &value);
+//                 insert(value);
+//                 break;
+//             case 2:
+//                 delete();
+//                 break;
+//             case 3:
+//                 display();
+//                 break;
+//             case 4:
+//                 return 0;
+//             default:
+//                 printf("Invalid choice.\n");
+//         }
+//     }
+// }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+void insert(int value){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode -> data = value;
+    newNode -> next = NULL;
+
+    if (head == NULL){
+        head = newNode;
+    }else{
+        struct Node* temp = head;
+        while(temp -> next != NULL){
+            temp = temp -> next;
+        }
+        temp -> next = newNode;
+    }
+    printf("%d is inserted\n", value);
+}
+
+void delete(){
+    if (head == NULL){
+        printf("The List is Empty\n");
+    }else{
+        struct Node* temp = head;
+        head = head -> next;
+        printf("%d is deleted\n", temp -> data);
+        free(temp);
+    }
+}
+
+void display(){
+    if(head == NULL){
+        printf("List Empty");
+    }else{
+        struct Node* temp = head;
+        printf("The Linked List is: ");
+        while(temp != NULL){
+            printf("%d -> ", temp -> data);
+            temp = temp -> next;
+        }
+    }
+}
